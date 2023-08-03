ec18397dc25213f19d08dab6faee77e3f33cd6e4
Thalles Araújo <thalleshnaraujo@brq.com>
2023-08-02 22:53:55 -0300

    Initial Commit

---
+++ Desafio CSTV Fuze/Desafio_CSTV_FuzeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_CSTV_FuzeApp.swift
+//  Desafio CSTV Fuze
+//
+//  Created by Thalles Araújo on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Desafio_CSTV_FuzeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
